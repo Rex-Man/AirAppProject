72ab63093e0def6ec10b32a7072ecfb9b83beff7
REX MAN (EUCD-EUC-ISD-OOCLL/ZHA) <rex.man@oocl.com>
2018-03-07 17:35:49 +0800

    add welcome page

--- app/src/main/java/com/manre/airappproject/WelcomeActivity.java
+++ app/src/main/java/com/manre/airappproject/WelcomeActivity.java
@@ -17,9 +17,28 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         coordinatorLayout=findViewById(R.id.coordinator);
+        initializePages();
 
+    }
+    private void initializePages() {
+        final WelcomeCoordinatorLayout coordinatorLayout
+                = (WelcomeCoordinatorLayout)findViewById(R.id.coordinator);
+        coordinatorLayout.addPage(R.layout.activity_welcome_first,
+                R.layout.activity_welcome_second);
+    }
+    private void initializeListeners() {
+        coordinatorLayout.setOnPageScrollListener(new WelcomeCoordinatorLayout.OnPageScrollListener() {
 
+            @Override
+            public void onScrollPage(View v, float progress, float maximum) {
 
+            }
+
+            @Override
+            public void onPageSelected(View v, int pageSelected) {
+
+            }
+        });
     }
 
 }
