0c14e2e467e08c4ee5481aa492dd7805377e8bc3
manre <rex.man@oocl.com>
2018-03-14 06:32:43 +0800

    add fragment in home search condition tab. processing

--- app/src/main/java/com/manre/airappproject/fragment/home/HomeFragment.java
+++ app/src/main/java/com/manre/airappproject/fragment/home/HomeFragment.java
@@ -6,10 +6,13 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.manre.airappproject.R;
 import com.manre.airappproject.common.GlideImageLoader;
@@ -85,8 +88,72 @@ public class HomeFragment extends Fragment {
     {
         mHomeSearchConditionTab.addTab(mHomeSearchConditionTab.newTab().setText(R.string.tab_home_search_one_way));
         mHomeSearchConditionTab.addTab(mHomeSearchConditionTab.newTab().setText(R.string.tab_home_search_round_trip));
-    }
+        mHomeSearchConditionTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
 
+                switch (tab.getPosition())
+                {
+                    case 0:
+                        Toast toast = Toast.makeText(getActivity(),"你点击了tab1", Toast.LENGTH_SHORT);//提示被点击了
+                        toast.show();
+                        break;
+                    case 1:
+                        Toast toast1 = Toast.makeText(getActivity(),"你点击了tab2", Toast.LENGTH_SHORT);//提示被点击了
+                        toast1.show();
+                        break;
+                }
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+//    public void add(Fragment fragment, int id,String tag){
+//        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        //优先检查，fragment是否存在，避免重叠
+//        BaseLibFragment tempFragment = (Fragment)fragmentManager.findFragmentByTag(tag);
+//        if(EmptyUtils.isNotEmpty(tempFragment)){
+//            fragment = tempFragment;
+//        }
+//        if(fragment.isAdded()){
+//            addOrShowFragment(fragmentTransaction,fragment,id,tag);
+//        }else{
+//            if(currentFragment!=null&&currentFragment.isAdded()){
+//                fragmentTransaction.hide(currentFragment).add(id, fragment,tag).commit();
+//            }else{
+//                fragmentTransaction.add(id, fragment,tag).commit();
+//            }
+//            currentFragment = fragment;
+//        }
+//    }
+//    /**
+//     * 添加或者显示 fragment
+//     *
+//     * @param fragment
+//     */
+//    private void addOrShowFragment(FragmentTransaction transaction, BaseLibFragment fragment, int id,String tag) {
+//        if(currentFragment == fragment)
+//            return;
+//        if (!fragment.isAdded()) { // 如果当前fragment未被添加，则添加到Fragment管理器中
+//            transaction.hide(currentFragment).add(id, fragment,tag).commit();
+//        } else {
+//            transaction.hide(currentFragment).show(fragment).commit();
+//        }
+//        currentFragment.setUserVisibleHint(false);
+//        currentFragment =  fragment;
+//        currentFragment.setUserVisibleHint(true);
+//    }
 
     @Override
     public void onStart() {
